aba50e95e5f90315ac11e3a31ec4a0eca87e1204
Akshansh Kumar <47569718+akshanshkmr@users.noreply.github.com>
2020-03-21 20:42:37 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,75 +1,57 @@
 from pynput.keyboard import Key, Controller
 import time
 
-comments="good"    #Enter your comments here 
-rating=3           #1=Strongly Disagree, 2=Disagree, 3=Neutral, 4=Agree, 5=Strongly Agree
-wait=3             #Time you need between running the program and selecting the faculty
+comments = "good"   # Enter your comments here
+rating = 5          # 1=Strongly Disagree, 2=Disagree, 3=Neutral, 4=Agree, 5=Strongly Agree
+wait = 3            # Time you need between running the program and selecting the faculty
+type = 'External'   # Choose between Internal and External feedback
 
 print("Amizone feedback tool (2019), By: Akshansh Kumar,AIIT")
 print("Now Select a faculty for feedback, and give the first rating")
-while(wait>0):
-    print("Select within:",wait,"Seconds")
+while (wait > 0):
+    print("Select within:", wait, "Seconds")
     time.sleep(1)
-    wait-=1
-
-keyboard=Controller()
+    wait -= 1
+keyboard = Controller()
 time.sleep(1)
-for i in range(2):
+
+def fill(n):
+    for i in range(n):
+        keyboard.press(Key.tab)
+        keyboard.release(Key.tab)
+        for j in range(rating):
+            keyboard.press(Key.left)
+            keyboard.release(Key.left)
     keyboard.press(Key.tab)
     keyboard.release(Key.tab)
-    for j in range(rating):
-        keyboard.press(Key.left)
-        keyboard.release(Key.left)
 
-keyboard.press(Key.tab)
-keyboard.release(Key.tab)
+def yes_no():
+    for i in range(3):
+        for j in range(1):
+            keyboard.press(Key.left)
+            keyboard.release(Key.left)
+        keyboard.press(Key.tab)
+        keyboard.release(Key.tab)
 
-for i in range(7):
-    keyboard.press(Key.tab)
-    keyboard.release(Key.tab)
-    for j in range(rating):
-        keyboard.press(Key.left)
-        keyboard.release(Key.left)
+def external_feedback():
+    fill(3)
+    fill(7)
+    fill(6)
+    fill(4)
+    fill(4)
+    yes_no()
+    keyboard.type(comments)
 
-keyboard.press(Key.tab)
-keyboard.release(Key.tab)
+def internal_feedback():
+    fill(2)
+    fill(7)
+    fill(4)
+    fill(2)
+    fill(4)
+    yes_no()
+    keyboard.type(comments)
 
-for i in range(4):
-    keyboard.press(Key.tab)
-    keyboard.release(Key.tab)
-    for j in range(rating):
-        keyboard.press(Key.left)
-        keyboard.release(Key.left)
-
-
-keyboard.press(Key.tab)
-keyboard.release(Key.tab)
-
-for i in range(2):
-    keyboard.press(Key.tab)
-    keyboard.release(Key.tab)
-    for j in range(rating):
-        keyboard.press(Key.left)
-        keyboard.release(Key.left)
-
-keyboard.press(Key.tab)
-keyboard.release(Key.tab)
-
-for i in range(4):
-    keyboard.press(Key.tab)
-    keyboard.release(Key.tab)
-    for j in range(rating):
-        keyboard.press(Key.left)
-        keyboard.release(Key.left)
-
-for i in range(4):
-    keyboard.press(Key.tab)
-    keyboard.release(Key.tab)
-    keyboard.press(Key.down)
-    keyboard.release(Key.down)
-
-keyboard.press(Key.tab)
-keyboard.release(Key.tab)
-    
-keyboard.type(comments)
-
+if(type=='External'):
+    external_feedback()
+elif(type=='Internal'):
+    internal_feedback()
